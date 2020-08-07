901959cbc51b6a555064ac631ed7e1d0c840f9b0
Ville Komulainen <villeko05@hotmail.com>
2020-08-07 12:37:18 +0300

    1.2

--- Osa1/kurssitiedot/part1/src/index.js
+++ Osa1/kurssitiedot/part1/src/index.js
@@ -13,15 +13,20 @@ const App = () => {
   return (
     <div>
       <Header course={course}/>
-      <Content part={part1} exercises={exercises1}/>
-      <Content part={part2} exercises={exercises2}/>
-      <Content part={part3} exercises={exercises3}/>
+      <Content part1={part1} exercise1={exercises1}
+      part2={part2} exercise2={exercises2}
+      part3={part3} exercise3={exercises3}/>
+      
       <Total result={exercises1+exercises2+exercises3}/>
     </div>
   )
 }
 
-
+const Part = (props) => {
+  return(
+    <p>{props.part} {props.exercise}</p>
+  )
+}
 
 
 const Header = (props) => {
@@ -39,10 +44,12 @@ const Total = (props) => {
 
 const Content = (props) => {
   return (
-    <p>
-      {props.part} {props.exercises}
-      
-    </p>
+    <div>
+      <Part part={props.part1} exercise={props.exercise1}/>
+      <Part part={props.part2} exercise={props.exercise2}/>
+      <Part part={props.part3} exercise={props.exercise3}/>
+    </div>
+    
     
   )
   
